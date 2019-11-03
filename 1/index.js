f09da04754eeacbd4b13a18c9952ff265ee81711
Max Oehme <maxoehme@yahoo.de>
2019-11-03 14:32:42 +0100

    as part for excersise 2 task 1: Added support for a linter. Choose Prettier since it is built into WebStorm IDE and easy to set up
    Also applied the formatting to exercise 1 files.

--- 1/index.js
+++ 1/index.js
@@ -1,13 +1,12 @@
-
-Vue.component('todo-list', {
-	props: ['todo', 'index'],
-	data() {
-		return {
-			isEditing: false,
-			temp: ''
-		}
-	},
-	template: `
+Vue.component("todo-list", {
+  props: ["todo", "index"],
+  data() {
+    return {
+      isEditing: false,
+      temp: ""
+    };
+  },
+  template: `
 				<li>
 					<div v-if="!isEditing">
 						{{ todo.message }}
@@ -21,55 +20,55 @@ Vue.component('todo-list', {
 					</div>
 				</li>
 	`,
-	methods: {
-		edit: function() {
-			this.temp = this.todo.message;
-			this.isEditing = true;
-		},
-		delToDo: function() {
-			this.isEditing = false;
-			vue.delete(this.index);
-		},
-		save: function() {
-			this.isEditing = false;
-			vue.save(this.temp, this.index);
-		},
-		cancel: function() {
-			this.isEditing = false;
-		}
-	}
-})
+  methods: {
+    edit: function() {
+      this.temp = this.todo.message;
+      this.isEditing = true;
+    },
+    delToDo: function() {
+      this.isEditing = false;
+      vue.delete(this.index);
+    },
+    save: function() {
+      this.isEditing = false;
+      vue.save(this.temp, this.index);
+    },
+    cancel: function() {
+      this.isEditing = false;
+    }
+  }
+});
 
 var vue = new Vue({
-	el: '#app',
-	data: {
-		message: 'Hello Vue!',
-		todos: [
-		  { id: '1', message: 'Foo', },
-		  { id: '2', message: 'Bar', },
-		  { id: '3', message: 'Baz', }
-		],
-		//nextID: '',
-	},
-	methods: {
-		add: function () {
-			this.todos.push({
-				id: this.todos.length + 1,
-				message: 'New ToDo'
-			});
-			this.todos.length + 1;
-		},
-		delete: function(index) {
-			this.todos.splice(index, 1);
-		},
-		save: function(msg, index) {
-			this.todos[index].message = msg;
-		}
-	},
-	computed: {
-		nextID(){
-		  let max = Math.max(...this.todos.map(t => t.id), 0)
-		  return max + 1
-		},
-	}
-})
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+    todos: [
+      { id: "1", message: "Foo" },
+      { id: "2", message: "Bar" },
+      { id: "3", message: "Baz" }
+    ]
+    //nextID: '',
+  },
+  methods: {
+    add: function() {
+      this.todos.push({
+        id: this.todos.length + 1,
+        message: "New ToDo"
+      });
+      this.todos.length + 1;
+    },
+    delete: function(index) {
+      this.todos.splice(index, 1);
+    },
+    save: function(msg, index) {
+      this.todos[index].message = msg;
+    }
+  },
+  computed: {
+    nextID() {
+      let max = Math.max(...this.todos.map(t => t.id), 0);
+      return max + 1;
+    }
+  }
+});
