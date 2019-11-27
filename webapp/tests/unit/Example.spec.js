1a65864e5c957f669353d8b15b602117eeae2b93
Max Oehme <maxoehme@yahoo.de>
2019-11-27 15:14:33 +0100

    fixed data in tests
    because of vue-apollo we needed to set the data differently

--- webapp/tests/unit/Example.spec.js
+++
@@ -1,5 +0,0 @@
-describe("Component", () => {
-  test("is a Vue instance", () => {
-    expect(true).toBeTruthy();
-  });
-});
--- webapp/tests/unit/TodoList.spec.js
+++ webapp/tests/unit/TodoList.spec.js
@@ -1,40 +1,39 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import TodoList from "@/components/TodoList";
 import TodoItem from "@/components/TodoItem";
 
-let propsData;
+let propsData = {
+  todoListData: [
+    {
+      id: "1",
+      text: "Foo",
+      author: { name: "Max" }
+    },
+    {
+      id: "2",
+      text: "Bar",
+      author: { name: "Victor" }
+    },
+    {
+      id: "3",
+      text: "Baz",
+      author: { name: "Max" }
+    }
+  ],
+  lastID: 3
+};
+
+let wrapper;
 
 beforeEach(() => {
-  propsData = {
-    todoListData: [
-      {
-        id: "1",
-        text: "Foo",
-        author: { name: "Max" }
-      },
-      {
-        id: "2",
-        text: "Bar",
-        author: { name: "Victor" }
-      },
-      {
-        id: "3",
-        text: "Baz",
-        author: { name: "Max" }
-      }
-    ],
-    lastID: 3
-  };
+  wrapper = mount(TodoList, {
+    data: function() {
+      return propsData;
+    }
+  });
 });
 
 describe("TodoList", () => {
-  let wrapper;
-  beforeAll(() => {
-    wrapper = shallowMount(TodoList, {
-      propsData
-    });
-  });
-
   it("contains 3 items", function() {
     expect(wrapper.findAll(TodoItem)).toHaveLength(3);
   });
