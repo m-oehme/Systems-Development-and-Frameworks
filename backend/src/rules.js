c91194cc734221ed2cb10c5d89d03a2e41960509
Max Oehme <maxoehme@yahoo.de>
2019-12-22 16:20:45 +0100

    fixed comment

--- backend/src/rules.js
+++ backend/src/rules.js
@@ -1,12 +1,13 @@
 const { rule, shield } = require("graphql-shield");
 
-// Rules
+// Roles
 const isAuthenticated = rule({ cache: "contextual" })(
   async (parent, args, ctx) => {
     return ctx.user !== null;
   }
 );
 
+// Permission
 const permissions = shield({
   Query: {
     todos: isAuthenticated
