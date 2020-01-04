aa1f69d3f5b8f2014fb51a835a2fcec7075bddc8
Max Oehme <maxoehme@yahoo.de>
2020-01-04 18:13:05 +0100

    new newsfeed on the frontend

---
+++ webapp/src/components/NewsFeed.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="newsfeed">
+    <h2 id="newsfeed_head">
+      NewsFeed!
+    </h2>
+    <ol>
+      <NewsItem v-for="news in this.newsfeed" :news="news" :key="news._id" />
+    </ol>
+  </div>
+</template>
+
+<script>
+import NewsItem from "./NewsItem";
+import gql from "graphql-tag";
+
+export default {
+  name: "NewsFeed",
+  components: { NewsItem },
+  props: ["user"],
+  apollo: {
+    newsfeed: {
+      query: gql`
+        query {
+          newsfeed: News {
+            _id
+            headline
+            author
+            createdAt
+          }
+        }
+      `
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ webapp/src/components/NewsItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="todo_item">
+    <div class="news_headline">{{ news.headline }}</div>
+    <div class="news_author">Created by {{ news.author }}</div>
+    <div class="news_date">{{ news.createdAt }}</div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "NewsItem",
+  props: ["news"]
+};
+</script>
+
+<style scoped></style>
